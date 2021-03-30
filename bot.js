d8cfeb5a1091fd83e010d6a0469db78f81275f98
Erwann Le Corff <45665066+erwannlecorff@users.noreply.github.com>
2021-03-30 18:52:08 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -4,8 +4,8 @@ const { prefix, token } = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var channelLol;
-//client.login(token); ancien token
-client.login(process.env.TOKEN);
+client.login(token);
+//client.login(process.env.TOKEN);
 var logger = require('winston');
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -14,7 +14,7 @@ logger.add(new logger.transports.Console, {
 });
 logger.level = 'debug';
 
-
+var apiKey = 'RGAPI-00d62c13-d8dc-48d5-b273-e646a8ea595a';
 
 
 client.on('ready', function (evt) {
@@ -25,20 +25,20 @@ client.on('ready', function (evt) {
 
 
 //checkTimeout();
- setInterval(()=>checkTimeout(),1200000);
+ setInterval(()=>checkTimeout(),60000);
 // mes variables 
 var routeApi= "https://euw1.api.riotgames.com";
 // info summoner apo
-var info = "https://euw1.api.riotgames.com/tft/summoner/v1/summoners/by-name/Chriskandar?api_key=RGAPI-20ba9075-a5d6-4e7e-8ce1-27c3d2025270";
+var info = "https://euw1.api.riotgames.com/tft/summoner/v1/summoners/by-name/Chriskandar?api_key=RGAPI-8aa18af1-6bf4-48f1-bc8d-f0e5650c0b29";
 //posible de virer id apo
 var apoID = "xhHNTNGYkdIOMQAE0ocH4G7gQQe2m0fYGTg9EWC7NghpZAw";
 
 //route rank apo 
-var rank = "https://euw1.api.riotgames.com/tft/league/v1/entries/by-summoner/xhHNTNGYkdIOMQAE0ocH4G7gQQe2m0fYGTg9EWC7NghpZAw?api_key=RGAPI-20ba9075-a5d6-4e7e-8ce1-27c3d2025270";
+var rank = "https://euw1.api.riotgames.com/tft/league/v1/entries/by-summoner/xhHNTNGYkdIOMQAE0ocH4G7gQQe2m0fYGTg9EWC7NghpZAw?api_key=";
 
 var apoOldTier = "PLATINUM";
-var apoOldRank = "IV";
-var apoOldLp = 60;
+var apoOldRank = "III";
+var apoOldLp = 3;
 
 var tabTier= new Array();
 tabTier["IRON"] =1;
@@ -57,21 +57,23 @@ tabRank["III"]=2;
 tabRank["II"]=3;
 tabRank["I"]=4;
 
-
+var Stonks="<:stonks:824979640287625308>";
+var notStonks="<:notstonks:824979555532931082>";
 
 
 client.on('message', message => {
-
+	channelLol= client.channels.cache.get('824327893287043122');
     // Notre client doit savoir s’il doit executer une commande
     //console.log(message.content);
-
     if (message.content.startsWith(prefix)) {
+        
         const args = message.content.slice(prefix.length).trim().split(/ +/);
-        const command = args.shift().toLowerCase();
+        const command = args.shift();
+        const complement = args.shift();
         switch(command) {
             // !intro
             case 'test':
-                message.channel.send('opérationnel');
+                message.channel.send('opérationnel '+Stonks+notStonks);
             break;
 
             case 'rank':
@@ -81,75 +83,65 @@ client.on('message', message => {
                 request.onreadystatechange = function() {
                     if (this.readyState == 4 && this.status == 200) {
                         var response = JSON.parse(this.responseText);
-                        rankAtm(response[0].tier,response[0].rank,response[0].leaguePoints,message.channel);
-
+                        message.channel.send('Apo est '+response[0].tier+' '+response[0].rank +' '+response[0].leaguePoints+' lp.');
                     }
                 }
-                request.open("GET", 'https://euw1.api.riotgames.com/tft/league/v1/entries/by-summoner/xhHNTNGYkdIOMQAE0ocH4G7gQQe2m0fYGTg9EWC7NghpZAw?api_key=RGAPI-20ba9075-a5d6-4e7e-8ce1-27c3d2025270');
+                request.open("GET", rank+apiKey);
                 request.send();
-
-                /*
+    
             break;  
-            case 'last':
-                //envoyer une requete à l'api 
-                var request = new XMLHttpRequest();
-                request.onreadystatechange = function() {
-                    if (this.readyState == 4 && this.status == 200) {
-                        var response = JSON.parse(this.responseText);
-                        checkTier(response[0].tier,response[0].rank,response[0].leaguePoints);
-                    }
-                }
-                request.open("GET", 'https://euw1.api.riotgames.com/tft/league/v1/entries/by-summoner/xhHNTNGYkdIOMQAE0ocH4G7gQQe2m0fYGTg9EWC7NghpZAw?api_key=RGAPI-20ba9075-a5d6-4e7e-8ce1-27c3d2025270');
-                request.send();
+            case 'set':
 
-
-            break; */  
+                apiKey= complement;
+                message.channel.send('api à jour : '+ apiKey);
+            break; 
                 
          }
      }
 });
-function rankAtm(tier,rank,lp,chan){
-    chan.send('Apo est '+tier+' '+rank +' '+lp+' lp.');
-    update(tier,rank,lp);
-}
-function checkTier(tier,rank,lp,chan){
+function checkTier(tier,rank,lp){
     if(tabTier[tier]>tabTier[apoOldTier]){
         //tier up 
-        chan.send('Apo a rank up il est maintenant '+tier +' '+rank +' '+lp+' lp. Il a gagné '+calculGainRankUp(lp)+' lp.');
+        channelLol.send('Apo a rank up il est maintenant '+tier +' '+rank +' '+lp+' lp. Il a gagné '+calculGainRankUp(lp)+' lp.');
+	channelLol.send(notStonks);
     }                        
     else if (tabTier[tier]==tabTier[apoOldTier]){
         checkRank(tier,rank,lp);
     }
     else{
         //tier down 
-        chan.send('Apo a demote il est maintenant '+tier +' '+rank +' '+lp+' lp. Il a perdu '+calculGain(lp)+' lp.');
-
+        channelLol.send('Apo a demote il est maintenant '+tier +' '+rank +' '+lp+' lp. Il a perdu '+calculGain(lp)+' lp.');
+	channelLol.send(Stonks);
     }
     update(tier,rank,lp);
 }
-function checkRank(tier,rank,lp,chan){
+function checkRank(tier,rank,lp){
     if(tabRank[rank]>tabRank[apoOldRank]){
-        chan.send('Apo a rank up il est maintenant '+tier +' '+rank +' '+lp+' lp. Il a gagné '+calculGainRankUp(lp)+' lp.');
+        channelLol.send('Apo a rank up il est maintenant '+tier +' '+rank +' '+lp+' lp. Il a gagné '+calculGainRankUp(lp)+' lp.');
+	channelLol.send(notStonks);
     }
     else if (tabRank[rank]==tabRank[apoOldRank]){
-        checkLp(lp);
+        checkLp(tier,rank,lp);
     }
     else{
-        chan.send('Apo a demote il est maintenant '+tier +' '+rank +' '+lp+' lp. Il a perdu '+calculGain(lp)+' lp.');
-    }
+        channelLol.send('Apo a demote il est maintenant '+tier +' '+rank +' '+lp+' lp. Il a perdu '+calculGain(lp)+' lp.');
+	channelLol.send(Stonks);	    
+}
     
 }    
-function checkLp(lp,chan) {
+function checkLp(tier,rank,lp) {
     if(lp > apoOldLp){
-        chan.send('Apo a gagné '+calculGain(lp)+' lp.');
+        channelLol.send('Apo a gagné '+calculGain(lp)+' lp. Il est maintenant '+tier +' '+rank +' '+lp+' lp.');
     }
     else if(lp<apoOldLp){
-        chan.send('Apo a perdu '+calculGain(lp)+' lp.');
+        channelLol.send('Apo a perdu '+calculGain(lp)+' lp. Il est maintenant '+tier +' '+rank +' '+lp+' lp.');
     }
 }
 function calculGain(lp){
-    if(apoOldLp==0)
+    if(apoOldLp==0 && lp >50)
         return Math.abs(100-lp);
+	else if (apoOldLp==0 && lp <50)
+	return lp;
     return Math.abs(lp - apoOldLp);
 }
 function calculGainRankUp(lp){
@@ -171,10 +163,10 @@ function checkTimeout(){
                 request.onreadystatechange = function() {
                     if (this.readyState == 4 && this.status == 200) {
                         var response = JSON.parse(this.responseText);
-                        checkTier(response[0].tier,response[0].rank,response[0].leaguePoints,channelLol);
+                        checkTier(response[0].tier,response[0].rank,response[0].leaguePoints);
                     }
                 }
-    request.open("GET", 'https://euw1.api.riotgames.com/tft/league/v1/entries/by-summoner/xhHNTNGYkdIOMQAE0ocH4G7gQQe2m0fYGTg9EWC7NghpZAw?api_key=RGAPI-20ba9075-a5d6-4e7e-8ce1-27c3d2025270');
+    request.open("GET", rank+apiKey);
     request.send();
    
    
